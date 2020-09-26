4f5a3424f45db508ded53320ef7349da0773b5e2
Sai-Ram7 <70585254+Sai-Ram7@users.noreply.github.com>
2020-09-26 16:38:28 +0530

    update

---
+++ 26-9-20(angualar tree)/tree/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component , Injectable } from '@angular/core';
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+import {BehaviorSubject , observable, of as observableOf} from 'rxjs';
+
+export class FileNode {
+  children : FileNode [];
+  filename: string ;
+  type : any;
+}
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nestedTreeControl: NestedTreeControl<FileNode>;
+  nestedDataSource: MatTreeNestedDataSource<FileNode>;
+  dataChange: BehaviorSubject<FileNode[]> = new BehaviorSubject<FileNode[]>([]);
+  title = 'tree';
+
+  constructor() {
+    this.nestedTreeControl = new NestedTreeControl<FileNode>(this._getChildren);
+    this.nestedDataSource = new MatTreeNestedDataSource();
+
+    this.dataChange.subscribe(data => this.nestedDataSource.data = data);
+
+    this.dataChange.next([
+      {
+        filename: "folder",
+        type: "",
+        children: [
+          {
+            filename: "test3",
+            type: "exe",
+            children: [],
+          }
+        ],
+      },
+      {
+        filename: "test2",
+        type: "exe",
+        children: [],
+      },
+    ]);
+  }
+
+  private _getChildren = (node: FileNode) => { return observableOf(node.children); };
+  
+  hasNestedChild = (_: number, nodeData: FileNode) => {return !(nodeData.type); };
+}

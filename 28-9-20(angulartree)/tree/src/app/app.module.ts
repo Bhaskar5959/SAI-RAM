6fae40db68d2e1339abd2ca409c4ad25826f80b5
Sai-Ram7 <70585254+Sai-Ram7@users.noreply.github.com>
2020-09-29 09:31:14 +0530

    update

---
+++ 28-9-20(angulartree)/tree/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TreeGridModule } from '@syncfusion/ej2-angular-treegrid';
+import{
+  PageService,
+  SortService,
+  FilterService
+} from "@syncfusion/ej2-angular-treegrid"
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    TreeGridModule
+  ],
+  providers: [PageService,
+    SortService,
+    FilterService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
